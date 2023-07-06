0f03104c949e17f388bf2738c24d0cb6ded0757a
Potoff <services.ankaamusic@gmail.com>
2023-07-06 18:43:15 +0200

    add product to cart

--- src/components/Shop/Shop.vue
+++ src/components/Shop/Shop.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
-    import type { ProductInterface } from '../../interfaces/product.interface';
-    import ShopProductList from './ShopProductList.vue';
+import type { ProductInterface } from '../../interfaces/product.interface';
+import ShopProductList from './ShopProductList.vue';
 
-    defineProps<{
-        products: ProductInterface[];
-    }>()
+defineProps<{
+    products: ProductInterface[];
+}>()
+
+const emit = defineEmits<{
+    (e: 'addProductToCart', productId: number): void;
+}>();
+
 </script>
 
 <template>
     <div>
-        <ShopProductList :products="products"/>
+        <ShopProductList @add-product-to-cart="emit('addProductToCart', $event)" :products="products" />
     </div>
 </template>
 
--- src/data/product.ts
+++ src/data/product.ts
@@ -1,41 +1,48 @@
 export default [
     {
+        id: 1,
         image: "src/assets/images/burger.png",
         title: "burger",
         price: 15,
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
     },
     {
+        id: 2,
         image: "src/assets/images/carbo.png",
         title: "pâtes carbo",
         price: 10,
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
     },
     {
+        id: 3,
         image: "src/assets/images/durum.png",
         title: "durum",
         price: 8,
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
     },
     {
+        id: 4,
         image: "src/assets/images/pita.png",
         title: "pita",
         price: 5,
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
     },
     {
+        id: 5,
         image: "src/assets/images/pizza.png",
         title: "pizza",
         price: 12,
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
     },
     {
+        id: 6,
         image: "src/assets/images/poutine.png",
         title: "poutine",
         price: 10,
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
     },
     {
+        id: 7,
         image: "src/assets/images/tacos.png",
         title: "tacos",
         price: 8,
